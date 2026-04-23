623728d57656118e69c7302af203edcf58ae4041
FELIPE MANUEL ANDRACA <48803376@ort>
2026-04-23 10:39:06 -0300

    comensando eliminar cita

--- src/Components/AdministradorCitas/index.jsx
+++ src/Components/AdministradorCitas/index.jsx
@@ -2,6 +2,10 @@ import Cita from "../Cita";
 import "./administradorCitas.css";
 
 const AdministradorCitas = ({ citas, setCitas }) => {
+     const eliminarCita = (nombre) => {
+  const nuevasCitas = citas.filter(cita => cita.nombre !== nombreMascota);
+  setCitas(nuevasCitas);
+  }
   return (
     <div className="one-half column">
       <h2>Administra tus citas</h2>
@@ -12,6 +16,7 @@ const AdministradorCitas = ({ citas, setCitas }) => {
           fecha={element.fecha}
           hora={element.hora}
           sintoma={element.sintoma}
+          eliminarCita={eliminarCita}
         />
       ))}
     </div>
